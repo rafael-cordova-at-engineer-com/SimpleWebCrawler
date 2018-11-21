17bd062dab9016430630c806b7fb661caf93f91e
rafael-cordova-at-engineer-com <45222482+rafael-cordova-at-engineer-com@users.noreply.github.com>
2018-11-21 00:17:48 -0600

    File Creation
    skeleton

---
+++ SimpleWebCrawler.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Rafael O. Cordova
+ *
+ */
+public final class SimpleWebCrawler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Simple Web Crawler");
+
+	}
+
+}
